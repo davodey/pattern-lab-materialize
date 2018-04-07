1c9f4b400575c90b778446c142a6274efc4b24bc
David O'Dey <dodey@icloud.com>
2018-04-06 21:55:32 -0700

    updated materialize

--- _tasks/ftp.js
+++ _tasks/ftp.js
@@ -4,9 +4,13 @@ var sftp = require('gulp-sftp');
 gulp.task('ftp', function () {
     return gulp.src('pattern-lab/public/**/*')
         .pipe(sftp({
-            // host:  "your host",
-            // user:  "your host username",
-            // pass:  "your host password",
-            // remotePath: "your host path"
+            // host: '10.72.9.195',
+            // user: 'root',
+            // pass: 'hnapoi4716&&9012',
+            // remotePath: '/home/webroot/ux-framework'
+            host: '159.203.121.101',
+            user: 'root',
+            pass: 'j8912LKuuB$',
+            remotePath: '/home/webroot/material-lab'
         }));
 });
